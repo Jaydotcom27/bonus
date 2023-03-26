41e5b76d0b5bff9b6dbc1d6a94b61edf9d4371f8
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-03-26 14:35:12 -0400

    Fixed mapper naming

--- part1/test.sh
+++ part1/test.sh
@@ -6,8 +6,8 @@
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/hdfstest2/shot_logs.csv /part1/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../bonus/part1/mapper1.py -mapper ../../bonus/part1/mapper1.py  \
--file ../../bonus/part1/reducer1.py -reducer ../../bonus/part1/reducer1.py \
+-file ../../bonus/part1/mapper.py -mapper ../../bonus/part1/mapper.py  \
+-file ../../bonus/part1/reducer.py -reducer ../../bonus/part1/reducer.py \
 -input /part1/input/* -output /part1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /part1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
