208aa58f537ae27e596027ed4780a92fd9e8f63f
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-03-26 14:28:59 -0400

    updated route

--- part1/test.sh
+++ part1/test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/parking_violations_2023.csv /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/hdfstest2/shot_logs.csv /part1/input/
 -file ../../bonus/part1/mapper1.py -mapper ../../bonus/part1/mapper1.py  \
 -file ../../bonus/part1/reducer1.py -reducer ../../bonus/part1/reducer1.py \
 -input /part1/input/* -output /part1/output/
