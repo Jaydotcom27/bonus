4a467ad200421217602b463d41d106a76174e778
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-03-26 14:33:28 -0400

    added jar

--- part1/test.sh
+++ part1/test.sh
@@ -4,6 +4,8 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/hdfstest2/shot_logs.csv /part1/input/
+
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../../bonus/part1/mapper1.py -mapper ../../bonus/part1/mapper1.py  \
 -file ../../bonus/part1/reducer1.py -reducer ../../bonus/part1/reducer1.py \
 -input /part1/input/* -output /part1/output/
