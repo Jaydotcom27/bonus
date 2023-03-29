ee0b27923b5b83fa2776be38473ed5face19a28a
Enmanuel Aquino <enmanuel1118@gmail.com>
2023-03-29 13:29:13 -0400

    try

--- part1/test.sh
+++ part1/test.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/hdfstest1/nyc_parking_violations_data.csv /part1/input/
+
+
+/usr/local/hadoop/bin/hdfs dfs -rm -r /bonus/part1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /bonus/part1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /bonus/part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/hdfstest2/shot_logs.csv /part2/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../bonus/part1/mapper.py -mapper ../../bonus/part1/mapper.py  \
--file ../../bonus/part1/reducer.py -reducer ../../bonus/part1/reducer.py \
--input /part1/input/* -output /part1/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /part1/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
+-file mapper.py -mapper mapper.py \
+-file reducer.py -reducer reducer.py \
+-input /bonus/part1/input/* -output /bonus/part1/output/
+
+echo "--------------------------- Question: What is the probability that it will get an ticket?? ---------------------------" 
+
+/usr/local/hadoop/bin/hdfs dfs -cat /bonus/part1/output/part-00000
+
+/usr/local/hadoop/bin/hdfs dfs -rm -r /bonus/part1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /bonus/part1/output/
 ../../stop.sh
+
